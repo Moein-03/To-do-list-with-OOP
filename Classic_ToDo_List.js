a751a9143e251644272f9ca00a96bbe7caf047a9
Moein <117684418+Moein-03@users.noreply.github.com>
2022-12-06 18:51:46 +0330

    Classic To do list_js
    This is js source code file , I hope this source code can be efficient for you ...

---
+++ Classic_ToDo_List.js
@@ -0,0 +1,106 @@
+class Task {
+    constructor(TaskTitle) {
+        this.Title = TaskTitle;
+        this.condition = false;
+    }
+}
+
+class ToDo  {
+    constructor(TaskContainer) {
+        this.todos = JSON.parse(localStorage.getItem('ToDos')) || [];
+        this.container = TaskContainer;
+        this.addButton = document.getElementById('addItem');
+        this.clearButton = document.getElementById('clearItems');
+        this.InputTxt = document.getElementById('txt');
+
+        this.render()
+    }
+
+    render() {
+        this.addButton.addEventListener('click', () => {
+            if(this.InputTxt.value != "") {
+                this.AddNewItem(this.InputTxt.value)
+            }
+        });
+
+        window || document.documentElement || document.body.addEventListener('keyup', e => {
+            if(this.InputTxt.value != "" && e.which == 13) {
+                this.AddNewItem(this.InputTxt.value)
+            }
+        });
+
+        this.clearButton.addEventListener('click' , () => {
+            this.DeleteAllItems()
+        });
+
+        this.ToDoInList();
+        this.saveToDoInStorage();
+    }
+
+    AddNewItem(textVal) {
+        if(textVal.trim() && isNaN(textVal)) {
+            this.InputTxt.value = '';
+
+            this.todos.push(new Task(textVal));
+
+            this.saveToDoInStorage();
+            this.render();
+        }
+    }
+
+    saveToDoInStorage() {
+        localStorage.setItem('ToDos', JSON.stringify(this.todos));
+    }
+
+    DeleteAllItems() {
+        this.todos = [];
+
+        this.saveToDoInStorage();
+        this.render();
+    }
+
+    ToDoInList() {
+        this.container.innerHTML = "";
+
+        this.todos.forEach(element => {
+            let MainDiv = document.createElement('div');
+            MainDiv.className = 'Item';
+            
+            let TextDiv = document.createElement('div');
+            TextDiv.className = 'ItemText';
+            TextDiv.innerHTML = element.Title;
+            
+            let CompeleteBtn = document.createElement('button');
+            CompeleteBtn.className = 'CompeleteItem';
+    
+            element.condition ? TextDiv.classList.add('compeleteCondition') : TextDiv.classList.remove('compeleteCondition');
+            element.condition ? (CompeleteBtn.innerHTML = `Uncompelete`) : (CompeleteBtn.innerHTML = `Compelete`);
+
+            CompeleteBtn.addEventListener('click', () => {
+                element.condition = !element.condition;
+
+                this.saveToDoInStorage();
+                this.ToDoInList();
+            })
+
+            let DeleteBtn = document.createElement('button');
+            DeleteBtn.className = 'DeleteItem';
+            DeleteBtn.innerHTML = 'Delete';
+
+            DeleteBtn.addEventListener('click', () => {
+                let DeletedItem = this.todos.findIndex(ThisToDo => ThisToDo == element);
+
+                this.todos.splice(DeletedItem , 1);
+
+                this.saveToDoInStorage();
+                this.ToDoInList();
+            })
+
+            MainDiv.append(TextDiv,CompeleteBtn,DeleteBtn);
+
+            this.container.append(MainDiv);
+        });
+    }
+}
+
+new ToDo(document.getElementById('sec2'));
